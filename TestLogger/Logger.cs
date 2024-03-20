0121cacedd73d0f3369d49b05d3a7161bb59eda3
Varren999 <varren999@gmail.com>
2024-03-20 15:35:20 +0700

    begin project

---
+++ TestLogger/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace TestLogger
+{
+    internal class Logger
+    {
+        public static void System(string text)
+        {
+            Message("System: " + text);
+        }
+        public static void Error(string text)
+        {
+            Message("Error: " + text);
+        }
+
+        // Метод для логирования.
+        private static void Message(string text)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("log.txt", true))
+            {
+                writer.WriteLine(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + ": " + text);
+                writer.Flush();
+            }
+        }
+    }
+}
---
+++ TestLogger/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.System("Запуск программы");
+        }
+    }
+}
